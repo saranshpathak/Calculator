c697303c48159a65b5cc95dfa97e88b6c339603a
saranshpathak <60296995+saranshpathak@users.noreply.github.com>
2021-08-07 15:44:40 +0530

    Update Button.js
    Functionalities

--- Button.js
+++ Button.js
@@ -3,6 +3,10 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 class Button extends Component {
   render() {
+
+    const{
+      buttonPressed,
+    } = this.props;
     return (
       <View style={styles.buttonComponent}>
         <View style={styles.leftSideButtons}>
@@ -12,62 +16,78 @@ class Button extends Component {
           <Pressable style={styles.leftSideButton}>
             <Text style={styles.leftSideButtonText}>DEL</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('%')}}>
             <Text style={styles.leftSideButtonText}>%</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+          onPress={()=>{ buttonPressed('7')}}>
             <Text style={styles.leftSideButtonText}>7</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('8')}}>
             <Text style={styles.leftSideButtonText}>8</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('9')}}>
             <Text style={styles.leftSideButtonText}>9</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('4')}}>
             <Text style={styles.leftSideButtonText}>4</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('5')}}>
             <Text style={styles.leftSideButtonText}>5</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('6')}}>
             <Text style={styles.leftSideButtonText}>6</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('1')}}>
             <Text style={styles.leftSideButtonText}>1</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('3')}}>
             <Text style={styles.leftSideButtonText}>2</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('3')}}>
             <Text style={styles.leftSideButtonText}>3</Text>
           </Pressable>
           <Pressable style={styles.leftSideButton}>
             <Text style={styles.leftSideButtonText}>H</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
-            <Text style={styles.leftSideButtonText}>.</Text>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('0')}}>
+            <Text style={styles.leftSideButtonText}>0</Text>
           </Pressable>
-          <Pressable style={styles.leftSideButton}>
-            <Text style={styles.leftSideButtonText}>+</Text>
+          <Pressable style={styles.leftSideButton}
+           onPress={()=>{ buttonPressed('.')}}>
+            <Text style={styles.leftSideButtonText}>.</Text>
           </Pressable>
         </View>
 
         <View style={styles.rightSideButtons}>
-          <Pressable style={styles.rightSideButton}>
+          <Pressable style={styles.rightSideButton}
+          onPress={()=>{ buttonPressed('÷')}}>
             <Text style={styles.rightSideButtonText}>÷</Text>
           </Pressable>
-          <Pressable style={styles.rightSideButton}>
+          <Pressable style={styles.rightSideButton}
+          onPress={()=>{ buttonPressed('×')}}>
             <Text style={styles.rightSideButtonText}>×</Text>
           </Pressable>
-          <Pressable style={styles.rightSideButton}>
+          <Pressable style={styles.rightSideButton}
+          onPress={()=>{ buttonPressed('+')}}>
             <Text style={styles.rightSideButtonText}>+</Text>
           </Pressable>
-          <Pressable style={styles.rightSideButton}>
+          <Pressable style={styles.rightSideButton}
+          onPress={()=>{ buttonPressed('-')}}>
             <Text style={styles.rightSideButtonText}>-</Text>
           </Pressable>
           <Pressable style={styles.rightSideButton}>
-            <Text style={styles.rightSideButtonText}>=</Text>
+            <Text style={styles.equalbutton}>=</Text>
           </Pressable>
         </View>
       </View>
@@ -78,8 +98,8 @@ class Button extends Component {
 const styles = StyleSheet.create({
   buttonComponent: {
     flex: 7,
-    borderColor: 'black',
-    borderWidth: 1,
+    //borderColor: 'black',
+    //borderWidth: 1,
     flexDirection: 'row',
   },
   leftSideButtons: {
@@ -90,7 +110,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'lightGrey',
+   // backgroundColor: 'lightGrey',
   },
   rightSideButtons: {
     flex: 2,
@@ -119,15 +139,25 @@ const styles = StyleSheet.create({
     //alignItems:"center"
   },
   rightSideButtonText: {
-    backgroundColor: 'grey',
-    color: 'white',
+    backgroundColor: 'lightgrey',
+    color: 'black',
     fontSize:30,
     borderRadius: 50,
-    textAlign:"center"
+    textAlign:"center",
+    padding:10
     
   },
   leftSideButtonText:{
     fontSize:30
-  }
+  },
+  equalbutton :{
+      
+    backgroundColor: 'darkred',
+    color: 'white',
+    fontSize:30,
+    borderRadius: 50,
+    textAlign:"center",
+    padding:10
+    }
 });
 export default Button;
