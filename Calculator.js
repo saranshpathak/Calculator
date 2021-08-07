fe8f65e9fae84c84b820a5d996dd7d85092ca284
saranshpathak <60296995+saranshpathak@users.noreply.github.com>
2021-08-07 15:47:28 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,22 +1,65 @@
-import React,{Component} from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 import Screen from './Screen';
-import Button from './Button'
- class Calculator extends Component{
-  render(){
-    return(
-      <View style ={styles.calculatorComponent}> <Screen />
-       <Button /> 
+import Button from './Button';
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    {
+      this.state = {
+        expression: 'Calculator',
+        result: '',
+      };
+    }
+  }
+
+  getButtonPressedValue = (buttonPressed) => {
+    const { expression, actualExpression, result } = this.state;
+    let newExpression = `${
+      expression === 'Calculator' ? '' : expression
+    }${buttonPressed}`;
+    this.setState({ expression: newExpression });
+
+     let actualCharacter = buttonPressed;
+    if (buttonPressed === '÷') {
+      actualCharacter = '/';
+    } else if(buttonPressed === '×') {
+      
+      actualCharacter='*';
+    } else if(buttonPressed === '−') {
+      actualCharacter = '-';
+    } else {
+
+    }
+    let newActualExpression = `${actualExpression}${actualCharacter}`;
+    this.setState({ actualExpression: newActualExpression });
+
+    try {
+      this.setState({
+        result: eval(newActualExpression).toString(),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  render() {
+    const { expression, result } = this.state;
+    return (
+      <View style={styles.calculatorComponent}>
+        {' '}
+        <Screen expression={expression} result={result} />
+        <Button buttonPressed={this.getButtonPressedValue} />
       </View>
     );
   }
 }
 
-
- const styles = StyleSheet.create({
-calculatorComponent:{
-flex :1,borderColor:'black',borderWidth:1
-}
+const styles = StyleSheet.create({
+  calculatorComponent: {
+    flex: 1,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
 });
 export default Calculator;
