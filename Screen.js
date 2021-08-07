abc16dad60f29cdc34144e3244396e3baa565e41
saranshpathak <60296995+saranshpathak@users.noreply.github.com>
2021-08-07 15:46:00 +0530

    Update Screen.js
    Functionalities Added

--- Screen.js
+++ Screen.js
@@ -3,8 +3,14 @@ import {View,StyleSheet,Text} from 'react-native';
 
 class Screen extends Component{
   render(){
+    const{
+      expression,
+      result
+    }= this.props;
     return(
-      <View style ={styles.screenComponent}> Screen</View>
+      <View style ={styles.screenComponent}> 
+      <Text style ={styles.expression}>  {expression}    </Text>
+      <Text style ={styles.result}> {result} </Text></View>
     );
   }
 }
@@ -12,7 +18,21 @@ class Screen extends Component{
 
 const styles = StyleSheet.create({
 screenComponent:{
-flex:3,borderColor:'black',borderWidth:1
-}
+flex:3,//borderColor:'black',borderWidth:1,
+backgroundColor:'darkred'
+},
+expression: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'right',
+    padding: 10,
+  },
+  result: {
+    color: 'white',
+    fontSize: 20,
+    // borderColor: 'white',
+    // borderWidth: 1,
+    textAlign: 'right',
+    padding: 10,},
 });
 export default Screen;
